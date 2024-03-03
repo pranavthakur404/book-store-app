52db17af7fb1b5dbbf7eee6531b13d87ca99de15
pranavthakur404 <pranavufc@gmail.com>
2024-03-04 01:31:05 +0530

    Responsive Added

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,17 @@ import Header from "./components/Header";
 import Nav from "./components/Nav";
 import { register } from "swiper/element/bundle";
 import Footer from "./components/Footer";
+import SearchBooks from "./components/SearchBooks";
 
 const App = () => {
   register();
   return (
     <div>
       <Header />
+      <span className="appSerchBar">
+        <SearchBooks />
+      </span>
+
       <Nav />
       <main>
         <Outlet />
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,13 +3,12 @@ import Logo from "./Logo";
 import styles from "../component_css/Header.module.css";
 import SearchBooks from "./SearchBooks";
 import { FaShoppingCart } from "react-icons/fa";
-import { Link } from "react-router-dom";
 import { useCartContext } from "../context/CartContext";
 import { useNavigate } from "react-router-dom";
 import { useAuthContext } from "../context/AuthProvider";
 
 const Header = () => {
-  const { login, setIsLogin } = useAuthContext();
+  const { setIsLogin } = useAuthContext();
   const navigate = useNavigate();
   const { cartList } = useCartContext();
 
@@ -27,13 +26,12 @@ const Header = () => {
         <div className={styles.headerContainer}>
           <div className={styles.logoContainer}>
             <Logo />
-            <SearchBooks />
+            <span className={styles.headerSearchbar}>
+              <SearchBooks />
+            </span>
           </div>
 
           <div className={styles.buttonContainer}>
-            <p>
-              <Link>FAQ</Link>
-            </p>
             <button onClick={handleCartBtn} className={styles.cartButton}>
               <FaShoppingCart className={styles.cartIcon} />
               {cartList.length > 0 && (
